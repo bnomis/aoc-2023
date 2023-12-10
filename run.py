8c6669ec8a6eb7744d5be01ffce36410872c0fa2
Simon Blanchard <bnomis@gmail.com>
2023-12-10 09:49:08 +0100

    add day07 to parent runner

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import day03.run
 import day04.run
 import day05.run
 import day06.run
+import day07.run
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
     day04.run.main()
     day05.run.main()
     day06.run.main()
+    day07.run.main()
 
 
 if __name__ == '__main__':
